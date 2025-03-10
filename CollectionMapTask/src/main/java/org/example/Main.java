78009cd8901f1d37aa7d5f25fc062063ac2e9499
rshahhyhy <144206614+rshahhyhy@users.noreply.github.com>
2025-03-10 13:31:48 +0300

    Fixed comments in CollectionMapTask

--- CollectionMapTask/src/main/java/org/example/Main.java
+++ CollectionMapTask/src/main/java/org/example/Main.java
@@ -7,13 +7,17 @@ public class Main {
 
     public static void main(String[] args) {
         List<String> list = List.of("bad", "bad", "bad", "bad", "bad", "good", "good", "good", "good", "normal");
+        List<Long> longList = List.of(1L, 2L, 3L, 4L, 5L, 6L, 7L);
+        List<Object> objectList = List.of(1, 2, 3L, 1, 3L, 3L, "bad", "bad");
         System.out.println(counterElements(list));
+        System.out.println(counterElements(longList));
+        System.out.println(counterElements(objectList));
     }
 
-    public static HashMap<String, Object> counterElements(List<?> arrayList) {
-        HashMap<String, Object> resultMap = new HashMap<>();
+    public static HashMap<String, Long> counterElements(List<?> arrayList) {
+        HashMap<String, Long> resultMap = new HashMap<>();
         for (Object o : arrayList) {
-            int counter = 0;
+            long counter = 0;
             for (Object object : arrayList) {
                 if (o.equals(object)) {
                     counter++;
