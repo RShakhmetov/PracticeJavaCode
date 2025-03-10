8e3cc8f021714db889babc6fdb9d92badd3151cf
rshahhyhy <144206614+rshahhyhy@users.noreply.github.com>
2025-03-10 14:02:33 +0300

    Fixed some comments

--- CollectionMapTask/src/main/java/org/example/Main.java
+++ CollectionMapTask/src/main/java/org/example/Main.java
@@ -1,29 +1,21 @@
 package org.example;
 
 import java.util.HashMap;
-import java.util.List;
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) {
-        List<String> list = List.of("bad", "bad", "bad", "bad", "bad", "good", "good", "good", "good", "normal");
-        List<Long> longList = List.of(1L, 2L, 3L, 4L, 5L, 6L, 7L);
-        List<Object> objectList = List.of(1, 2, 3L, 1, 3L, 3L, "bad", "bad");
-        System.out.println(counterElements(list));
-        System.out.println(counterElements(longList));
-        System.out.println(counterElements(objectList));
+        String[] array = {"bad", "bad", "bad", "bad", "bad", "good", "good", "good", "good", "normal"};
+        Integer[] arrayNumber = {1, 2, 3, 4, 5, 3, 2, 1};
+        System.out.println(counterElements(arrayNumber));
+        System.out.println(counterElements(array));
     }
 
-    public static HashMap<String, Long> counterElements(List<?> arrayList) {
-        HashMap<String, Long> resultMap = new HashMap<>();
-        for (Object o : arrayList) {
-            long counter = 0;
-            for (Object object : arrayList) {
-                if (o.equals(object)) {
-                    counter++;
-                }
-            }
-            resultMap.put(o.toString(), counter);
+    public static <T> Map<T, Integer> counterElements(T[] array) {
+        Map<T, Integer> resultMap = new HashMap<>();
+        for (T element : array) {
+            resultMap.merge(element, 1, Integer::sum);
         }
         return resultMap;
     }
